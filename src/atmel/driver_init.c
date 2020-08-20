f3cd84d8c7bc55ff041f69616d95c57509a1972b
Woody Wu <woody.wu2@honeywell.com>
2020-08-20 21:19:55 +0800

    initial commit

---
+++ src/atmel/driver_init.c
@@ -0,0 +1,344 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+
+#include <hpl_adc_base.h>
+#include <hpl_adc_base.h>
+#include <hpl_rtc_base.h>
+
+/* The channel amount for ADC */
+#define ADC_1_CH_AMOUNT 1
+
+/* The buffer size for ADC */
+#define ADC_1_BUFFER_SIZE 16
+
+/* The maximal channel number of enabled channels */
+#define ADC_1_CH_MAX 0
+
+struct adc_async_descriptor         ADC_1;
+struct adc_async_channel_descriptor ADC_1_ch[ADC_1_CH_AMOUNT];
+struct timer_descriptor             TIMER_1;
+struct spi_m_sync_descriptor        SPI_0;
+struct timer_descriptor             TIMER_0;
+
+static uint8_t ADC_1_buffer[ADC_1_BUFFER_SIZE];
+static uint8_t ADC_1_map[ADC_1_CH_MAX + 1];
+
+struct adc_sync_descriptor ADC_0;
+
+struct spi_m_async_descriptor SPI_1;
+
+struct usart_sync_descriptor USART_0;
+
+struct usart_sync_descriptor TARGET_IO;
+
+struct pwm_descriptor PWM_0;
+
+struct mac_async_descriptor ETHERNET_MAC_0;
+
+void ADC_0_PORT_init(void)
+{
+}
+
+void ADC_0_CLOCK_init(void)
+{
+	hri_mclk_set_APBDMASK_ADC0_bit(MCLK);
+	hri_gclk_write_PCHCTRL_reg(GCLK, ADC0_GCLK_ID, CONF_GCLK_ADC0_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+}
+
+void ADC_0_init(void)
+{
+	ADC_0_CLOCK_init();
+	ADC_0_PORT_init();
+	adc_sync_init(&ADC_0, ADC0, (void *)NULL);
+}
+
+/**
+ * \brief ADC initialization function
+ *
+ * Enables ADC peripheral, clocks and initializes ADC driver
+ */
+void ADC_1_init(void)
+{
+	hri_mclk_set_APBDMASK_ADC1_bit(MCLK);
+	hri_gclk_write_PCHCTRL_reg(GCLK, ADC1_GCLK_ID, CONF_GCLK_ADC1_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	adc_async_init(&ADC_1, ADC1, ADC_1_map, ADC_1_CH_MAX, ADC_1_CH_AMOUNT, &ADC_1_ch[0], (void *)NULL);
+	adc_async_register_channel_buffer(&ADC_1, 0, ADC_1_buffer, ADC_1_BUFFER_SIZE);
+}
+
+/**
+ * \brief Timer initialization function
+ *
+ * Enables Timer peripheral, clocks and initializes Timer driver
+ */
+static void TIMER_1_init(void)
+{
+	hri_mclk_set_APBAMASK_RTC_bit(MCLK);
+	timer_init(&TIMER_1, RTC, _rtc_get_timer());
+}
+
+void SPI_0_PORT_init(void)
+{
+
+	gpio_set_pin_level(PA04,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(PA04, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(PA04, PINMUX_PA04D_SERCOM0_PAD0);
+
+	gpio_set_pin_level(PA05,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(PA05, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(PA05, PINMUX_PA05D_SERCOM0_PAD1);
+
+	// Set pin direction to input
+	gpio_set_pin_direction(PA06, GPIO_DIRECTION_IN);
+
+	gpio_set_pin_pull_mode(PA06,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(PA06, PINMUX_PA06D_SERCOM0_PAD2);
+}
+
+void SPI_0_CLOCK_init(void)
+{
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM0_GCLK_ID_CORE, CONF_GCLK_SERCOM0_CORE_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM0_GCLK_ID_SLOW, CONF_GCLK_SERCOM0_SLOW_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+
+	hri_mclk_set_APBAMASK_SERCOM0_bit(MCLK);
+}
+
+void SPI_0_init(void)
+{
+	SPI_0_CLOCK_init();
+	spi_m_sync_init(&SPI_0, SERCOM0);
+	SPI_0_PORT_init();
+}
+
+void SPI_1_PORT_init(void)
+{
+
+	gpio_set_pin_level(PA00,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(PA00, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(PA00, PINMUX_PA00D_SERCOM1_PAD0);
+
+	gpio_set_pin_level(PA01,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(PA01, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(PA01, PINMUX_PA01D_SERCOM1_PAD1);
+
+	// Set pin direction to input
+	gpio_set_pin_direction(PA18, GPIO_DIRECTION_IN);
+
+	gpio_set_pin_pull_mode(PA18,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(PA18, PINMUX_PA18C_SERCOM1_PAD2);
+}
+
+void SPI_1_CLOCK_init(void)
+{
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM1_GCLK_ID_CORE, CONF_GCLK_SERCOM1_CORE_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM1_GCLK_ID_SLOW, CONF_GCLK_SERCOM1_SLOW_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+
+	hri_mclk_set_APBAMASK_SERCOM1_bit(MCLK);
+}
+
+void SPI_1_init(void)
+{
+	SPI_1_CLOCK_init();
+	spi_m_async_init(&SPI_1, SERCOM1);
+	SPI_1_PORT_init();
+}
+
+void USART_0_PORT_init(void)
+{
+
+	gpio_set_pin_function(PA09, PINMUX_PA09D_SERCOM2_PAD0);
+
+	gpio_set_pin_function(PA08, PINMUX_PA08D_SERCOM2_PAD1);
+}
+
+void USART_0_CLOCK_init(void)
+{
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM2_GCLK_ID_CORE, CONF_GCLK_SERCOM2_CORE_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM2_GCLK_ID_SLOW, CONF_GCLK_SERCOM2_SLOW_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+
+	hri_mclk_set_APBBMASK_SERCOM2_bit(MCLK);
+}
+
+void USART_0_init(void)
+{
+	USART_0_CLOCK_init();
+	usart_sync_init(&USART_0, SERCOM2, (void *)NULL);
+	USART_0_PORT_init();
+}
+
+void TARGET_IO_PORT_init(void)
+{
+
+	gpio_set_pin_function(PA17, PINMUX_PA17D_SERCOM3_PAD0);
+
+	gpio_set_pin_function(PA16, PINMUX_PA16D_SERCOM3_PAD1);
+}
+
+void TARGET_IO_CLOCK_init(void)
+{
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM3_GCLK_ID_CORE, CONF_GCLK_SERCOM3_CORE_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM3_GCLK_ID_SLOW, CONF_GCLK_SERCOM3_SLOW_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+
+	hri_mclk_set_APBBMASK_SERCOM3_bit(MCLK);
+}
+
+void TARGET_IO_init(void)
+{
+	TARGET_IO_CLOCK_init();
+	usart_sync_init(&TARGET_IO, SERCOM3, (void *)NULL);
+	TARGET_IO_PORT_init();
+}
+
+void delay_driver_init(void)
+{
+	delay_init(SysTick);
+}
+
+void PWM_0_PORT_init(void)
+{
+}
+
+void PWM_0_CLOCK_init(void)
+{
+
+	hri_mclk_set_APBAMASK_TC0_bit(MCLK);
+	hri_gclk_write_PCHCTRL_reg(GCLK, TC0_GCLK_ID, CONF_GCLK_TC0_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+}
+
+void PWM_0_init(void)
+{
+	PWM_0_CLOCK_init();
+	PWM_0_PORT_init();
+	pwm_init(&PWM_0, TC0, _tc_get_pwm());
+}
+
+/**
+ * \brief Timer initialization function
+ *
+ * Enables Timer peripheral, clocks and initializes Timer driver
+ */
+static void TIMER_0_init(void)
+{
+	hri_mclk_set_APBAMASK_TC1_bit(MCLK);
+	hri_gclk_write_PCHCTRL_reg(GCLK, TC1_GCLK_ID, CONF_GCLK_TC1_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+
+	timer_init(&TIMER_0, TC1, _tc_get_timer());
+}
+
+void ETHERNET_MAC_0_PORT_init(void)
+{
+}
+
+void ETHERNET_MAC_0_CLOCK_init(void)
+{
+	hri_mclk_set_AHBMASK_GMAC_bit(MCLK);
+	hri_mclk_set_APBCMASK_GMAC_bit(MCLK);
+}
+
+void ETHERNET_MAC_0_init(void)
+{
+	ETHERNET_MAC_0_CLOCK_init();
+	mac_async_init(&ETHERNET_MAC_0, GMAC);
+	ETHERNET_MAC_0_PORT_init();
+}
+
+void ETHERNET_MAC_0_example(void)
+{
+	mac_async_enable(&ETHERNET_MAC_0);
+	mac_async_write(&ETHERNET_MAC_0, (uint8_t *)"Hello World!", 12);
+}
+
+void system_init(void)
+{
+	init_mcu();
+
+	// GPIO on PB22
+
+	// Set pin direction to input
+	gpio_set_pin_direction(front_panel_di, GPIO_DIRECTION_IN);
+
+	gpio_set_pin_pull_mode(front_panel_di,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(front_panel_di, GPIO_PIN_FUNCTION_OFF);
+
+	ADC_0_init();
+	ADC_1_init();
+
+	TIMER_1_init();
+
+	SPI_0_init();
+
+	SPI_1_init();
+
+	USART_0_init();
+
+	TARGET_IO_init();
+
+	delay_driver_init();
+
+	PWM_0_init();
+
+	TIMER_0_init();
+	ETHERNET_MAC_0_init();
+}
---
+++ src/atmel/examples/driver_examples.c
@@ -0,0 +1,170 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_examples.h"
+#include "driver_init.h"
+#include "utils.h"
+
+/**
+ * Example of using ADC_0 to generate waveform.
+ */
+void ADC_0_example(void)
+{
+	uint8_t buffer[2];
+
+	adc_sync_enable_channel(&ADC_0, 0);
+
+	while (1) {
+		adc_sync_read_channel(&ADC_0, 0, buffer, 2);
+	}
+}
+
+static void convert_cb_ADC_1(const struct adc_async_descriptor *const descr, const uint8_t channel)
+{
+}
+
+/**
+ * Example of using ADC_1 to generate waveform.
+ */
+void ADC_1_example(void)
+{
+	adc_async_enable_channel(&ADC_1, 0);
+	adc_async_register_callback(&ADC_1, 0, ADC_ASYNC_CONVERT_CB, convert_cb_ADC_1);
+	adc_async_start_conversion(&ADC_1);
+}
+
+static struct timer_task TIMER_1_task1, TIMER_1_task2;
+/**
+ * Example of using TIMER_1.
+ */
+static void TIMER_1_task1_cb(const struct timer_task *const timer_task)
+{
+}
+
+static void TIMER_1_task2_cb(const struct timer_task *const timer_task)
+{
+}
+
+void TIMER_1_example(void)
+{
+	TIMER_1_task1.interval = 100;
+	TIMER_1_task1.cb       = TIMER_1_task1_cb;
+	TIMER_1_task1.mode     = TIMER_TASK_REPEAT;
+	TIMER_1_task2.interval = 200;
+	TIMER_1_task2.cb       = TIMER_1_task2_cb;
+	TIMER_1_task2.mode     = TIMER_TASK_REPEAT;
+
+	timer_add_task(&TIMER_1, &TIMER_1_task1);
+	timer_add_task(&TIMER_1, &TIMER_1_task2);
+	timer_start(&TIMER_1);
+}
+
+/**
+ * Example of using SPI_0 to write "Hello World" using the IO abstraction.
+ */
+static uint8_t example_SPI_0[12] = "Hello World!";
+
+void SPI_0_example(void)
+{
+	struct io_descriptor *io;
+	spi_m_sync_get_io_descriptor(&SPI_0, &io);
+
+	spi_m_sync_enable(&SPI_0);
+	io_write(io, example_SPI_0, 12);
+}
+
+/**
+ * Example of using SPI_1 to write "Hello World" using the IO abstraction.
+ *
+ * Since the driver is asynchronous we need to use statically allocated memory for string
+ * because driver initiates transfer and then returns before the transmission is completed.
+ *
+ * Once transfer has been completed the tx_cb function will be called.
+ */
+
+static uint8_t example_SPI_1[12] = "Hello World!";
+
+static void complete_cb_SPI_1(const struct spi_m_async_descriptor *const io_descr)
+{
+	/* Transfer completed */
+}
+
+void SPI_1_example(void)
+{
+	struct io_descriptor *io;
+	spi_m_async_get_io_descriptor(&SPI_1, &io);
+
+	spi_m_async_register_callback(&SPI_1, SPI_M_ASYNC_CB_XFER, (FUNC_PTR)complete_cb_SPI_1);
+	spi_m_async_enable(&SPI_1);
+	io_write(io, example_SPI_1, 12);
+}
+
+/**
+ * Example of using USART_0 to write "Hello World" using the IO abstraction.
+ */
+void USART_0_example(void)
+{
+	struct io_descriptor *io;
+	usart_sync_get_io_descriptor(&USART_0, &io);
+	usart_sync_enable(&USART_0);
+
+	io_write(io, (uint8_t *)"Hello World!", 12);
+}
+
+/**
+ * Example of using TARGET_IO to write "Hello World" using the IO abstraction.
+ */
+void TARGET_IO_example(void)
+{
+	struct io_descriptor *io;
+	usart_sync_get_io_descriptor(&TARGET_IO, &io);
+	usart_sync_enable(&TARGET_IO);
+
+	io_write(io, (uint8_t *)"Hello World!", 12);
+}
+
+void delay_example(void)
+{
+	delay_ms(5000);
+}
+
+/**
+ * Example of using PWM_0.
+ */
+void PWM_0_example(void)
+{
+	pwm_set_parameters(&PWM_0, 10000, 5000);
+	pwm_enable(&PWM_0);
+}
+
+static struct timer_task TIMER_0_task1, TIMER_0_task2;
+
+/**
+ * Example of using TIMER_0.
+ */
+static void TIMER_0_task1_cb(const struct timer_task *const timer_task)
+{
+}
+
+static void TIMER_0_task2_cb(const struct timer_task *const timer_task)
+{
+}
+
+void TIMER_0_example(void)
+{
+	TIMER_0_task1.interval = 100;
+	TIMER_0_task1.cb       = TIMER_0_task1_cb;
+	TIMER_0_task1.mode     = TIMER_TASK_REPEAT;
+	TIMER_0_task2.interval = 200;
+	TIMER_0_task2.cb       = TIMER_0_task2_cb;
+	TIMER_0_task2.mode     = TIMER_TASK_REPEAT;
+
+	timer_add_task(&TIMER_0, &TIMER_0_task1);
+	timer_add_task(&TIMER_0, &TIMER_0_task2);
+	timer_start(&TIMER_0);
+}
